4e893e9bc12ece8f6e5e7047b928140e9e59296a
Mike <mnicholas@albany.edu>
2017-02-12 20:18:03 -0500

    Weapon Class get/set methods
    Added get/set methods for the following weapon variables:
    String methods for weaponExamineDescription
    integer methods for weaponDamage

--- Weapon.java
+++ Weapon.java
@@ -1,5 +1,22 @@
 package rp;
 
 public class Weapon {
+String weaponExamineDescription;
+int weaponDamage;
 
+//sets the integer value for the amount of damage the weapon deals
+	public void setDamage(weaponDamage){
+	}
+//gets the integer value for the amount of damage the weapon deals
+	public int getDamage(){
+		return (weaponDamage);
+}
+//sets the weapon description as a String so that it shows up when examined	
+	public void setWeaponExamineDescription(weaponExamineDescription){
+		
+	}
+//gets the weapon description
+	public String getWeaponExamineDescription(){
+		return (weaponExamineDescription);
+	}
 }
