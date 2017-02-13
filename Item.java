05d66325107f6fa51f882d60a8e02b4274ba605d
Mike <mnicholas@albany.edu>
2017-02-12 19:30:10 -0500

    Fixed Item Upload
    Accidently uploaded the item.class instead of the item.java. Fixed now.

---
+++ Item.java
@@ -0,0 +1,16 @@
+package rp;
+
+public class Item {
+ String Name;
+ String examineDescription;
+ int Weight;
+ 
+	//sets the name of the item in question
+	public void setName(String Key){
+	}
+	//gets the name of the item in question
+	public String getName(){
+		return (String);
+	}
+
+}
