5b707b2508b9b11ca17eb4b3c3f1ef5a840b921b
Mike <mnicholas@albany.edu>
2017-02-12 19:47:07 -0500

    Added get/set methods Food Class
    Added get/set methods for the following credentials in the food class:
    nutrition (int)
    quantity (int)
    food description (String)

--- Food.java
+++ Food.java
@@ -1,5 +1,30 @@
 package rp;
 
 public class Food {
+	int nutrition;
+	int quantity;
+	String foodExamineDescription;
+	
+	//sets the description of the food in question as a string
+	public void setFoodExamineDescription(String){
+	}
+	//gets the food's description as a string
+	public String getFoodExamineDescription(){
+		return (foodExamineDescription);
+	}
+	//sets the nutritional value of the food using an integer
+	public void setNutrition(int){
+	}
+	//gets the nutritional value of the food
+	public int getNutrition(){
+		return (nutrition);
+	}
+	//sets the amount of food the player has as an integer value
+	public void setQuantity(int){
+	}
+	//gets the amount of food the player
+	public int getQuantity(){
+		return (quantity);
+	}
 
 }
