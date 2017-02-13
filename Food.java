335cb3f94b6b3d354f7800270ab2065ef041fb0e
Mike <mnicholas@albany.edu>
2017-02-12 20:48:44 -0500

    Public to Private
    Privatized the get/set methods for each of the classes

--- Food.java
+++ Food.java
@@ -6,24 +6,24 @@ public class Food {
 	String foodExamineDescription;
 	
 	//sets the description of the food in question as a string
-	public void setFoodExamineDescription(String){
+	private void setFoodExamineDescription(String){
 	}
 	//gets the food's description as a string
-	public String getFoodExamineDescription(){
+	private String getFoodExamineDescription(){
 		return (foodExamineDescription);
 	}
 	//sets the nutritional value of the food using an integer
-	public void setNutrition(int){
+	private void setNutrition(int){
 	}
 	//gets the nutritional value of the food
 	public int getNutrition(){
 		return (nutrition);
 	}
 	//sets the amount of food the player has as an integer value
-	public void setQuantity(int){
+	private void setQuantity(int){
 	}
 	//gets the amount of food the player
-	public int getQuantity(){
+	private int getQuantity(){
 		return (quantity);
 	}
 
