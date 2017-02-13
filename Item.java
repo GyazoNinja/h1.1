0585c11ca5aee5e8cd206e0d9bc82ed79b9d39f1
Mike <mnicholas@albany.edu>
2017-02-12 23:35:45 -0500

    Methods/Main Method Alteration
    Removed examine description method and changed constructors to reflect
    it. Fixed some of main method call methods to test it. Item class is
    functional. Remaining classes will be updated in a similar format.

--- Item.java
+++ Item.java
@@ -5,38 +5,32 @@ public class Item {
 	public String description;
 	public int weight;
 //constructor for item class. defines item name, description, and weight.
- public Item (String itemName, String examineDescription, int Weight){
-	itemName = name;
-	examineDescription = description;
-	Weight = weight;
+ public Item (String itemName, int Weight){
+	this.name = itemName;
+	this.weight = Weight;
 	 
  }
 //allows for super constructor in armor, weapon, and food classes
  public Item (){ 
  }
 	//sets the name of the item in question
-	private void setName(String name){
-		String itemName = name;
+	public void setName(String itemName){
+		this.name = itemName;
 	}
 	//gets the name of the item in question
-	private String getName(){
-		return name;
+	public String getName(){
+		return this.name;
 	}
 	//sets the description of the item in question
-	private void setExamineDescription(String Int){
-		String examineDescription = description;
-		int Weight = weight;	
-	}
-	//returns a string description of the item in question
-	private String getExamineDescription(){
-		return(description);
+	public void examineDescription(){
+		System.out.println("This is a " + this.name + ". It has a weight of " + this.weight + "." );
 	}
 	//gets the integer value for the weight of the item
-	private int getWeight (){
-	return(weight);
+	public int getWeight (){
+	return this.weight;
 	}
 	//sets the integer value for the weight of the item
-	private void setWeight(int Weight){
-	Weight = weight;
-}
+	public void setWeight(int Weight){
+	this.weight = Weight;
+	}
 }
--- Main.java
+++ Main.java
@@ -3,9 +3,25 @@ package rp;
 public class Main {
 
 	public static void main(String [] args){
-		String Container = null;
-		Item Box = new Item(Container, "A heavy container.", 10);
-		System.out.println(Box);
+
+		Item Box = new Item("Box", 100);
+		Box.examineDescription();
+		Box.getName();
+		System.out.println(Box.getWeight());
+		
+		Weapon BroadSword = new Weapon ("A heavy, two handed sword.", 100);
+		System.out.println(BroadSword.getDamage());
+		System.out.println(BroadSword.getWeaponExamineDescription());
+		
+		Armor Chainmail = new Armor("A thin, see-through plating that covers your torso.", 50);
+		System.out.println(Chainmail.getResistance());
+		Chainmail.getArmorDescription();
+		
+		Food Apple = new Food("A tasty snack found on a local farm.", 10, 10);
+		System.out.println(Apple.getQuantity());
+		System.out.println(Apple.getFoodExamineDescription());
 	}
 
 }
+
+
