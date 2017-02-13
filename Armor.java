eb31c64048eb7a2669fe4744c3bec27fd69ea106
Mike <mnicholas@albany.edu>
2017-02-12 20:21:42 -0500

    Armor get/set methods
    Added get/set methods for the following variables in the armor class:
    armorDescription (String)
    Resistance (int)

--- Armor.java
+++ Armor.java
@@ -1,5 +1,21 @@
 package rp;
 
 public class Armor {
+int Resistance;
+String armorDescription;
 
+//sets the resistance value of the armor as an integer
+	public void setResistance(Resistance){
+}
+//gets the resistance value of the armor
+	public int getResistance(){
+		return (Resistance);
+	}
+//sets the armor description of the armor when it is examined	
+	public void setArmorDescription(armorDescription){	
+	}
+//gets the armor description and displays it when examined	
+	public String getArmorDescription(){
+		return (armorDescription);
+	}
 }
